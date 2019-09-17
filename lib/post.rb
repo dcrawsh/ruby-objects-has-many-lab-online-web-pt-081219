d881c924bd3217078adccb6a4d10f08e9cde8c5b
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-17 20:02:50 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post 
+@@all = Array.new 
+attr_accessor :title , :author
+
+def initialize(title)
+@title = title 
+@@all << self 
+end 
+
+def self.all 
+@@all 
+end 
+
+
+def author_name 
+self.author.name 
+end 
+
+
+
+end 
